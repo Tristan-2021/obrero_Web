b0076de396223d0bccc61ae9414449fe127214be
jhon <Jhonmilerrons@outlook.eso>
2022-02-11 18:02:47 -0500

    19

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:nombreapp/ingore.dart';
-import 'package:nombreapp/src/clean_architecture/view/cubit/viwcubit/cubit_getdata/cubit/blog_cubit.dart';
 import 'example/homeApp/myapp.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await FirebaseInitial.initialFirebase();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: 'AIzaSyB7zYuXd8UbryfgvtM-BgkBPrtISi20iHI',
+    authDomain: 'blogabogado-fc776.firebaseapp.com',
+    projectId: 'blogabogado-fc776',
+    storageBucket: 'blogabogado-fc776.appspot.com',
+    messagingSenderId: '276349583756',
+    appId: '1:276349583756:web:b38e890173335f4dddd6a9',
+    measurementId: 'G-VER4XZS1JW',
+  ));
   GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
   runApp(const MyApp());
 }
