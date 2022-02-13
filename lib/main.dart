ee7523db8c578c079d459e0e5f739f33ce0de193
jhon <Jhonmilerrons@outlook.eso>
2022-02-13 14:46:06 -0500

    30

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'example/homeApp/myapp.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       options: const FirebaseOptions(
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
+    apiKey: 'AIzaSyB7zYuXd8UbryfgvtM-BgkBPrtISi20iHI',
+    authDomain: 'blogabogado-fc776.firebaseapp.com',
+    projectId: 'blogabogado-fc776',
+    storageBucket: 'blogabogado-fc776.appspot.com',
+    messagingSenderId: '276349583756',
+    appId: '1:276349583756:web:b38e890173335f4dddd6a9',
+    measurementId: 'G-VER4XZS1JW',
   ));
- // GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+  // GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
   runApp(const MyApp());
 }
