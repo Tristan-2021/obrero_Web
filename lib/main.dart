9e8dc4cb1d172aa3a3135b5aac21dd23cb30a72a
Tristan-2021 <79274889+Tristan-2021@users.noreply.github.com>
2022-02-13 13:54:28 -0500

    29

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       options: const FirebaseOptions(
-    apiKey: 'AIzaSyB7zYuXd8UbryfgvtM-BgkBPrtISi20iHI',
-    authDomain: 'blogabogado-fc776.firebaseapp.com',
-    projectId: 'blogabogado-fc776',
-    storageBucket: 'blogabogado-fc776.appspot.com',
-    messagingSenderId: '276349583756',
-    appId: '1:276349583756:web:b38e890173335f4dddd6a9',
-    measurementId: 'G-VER4XZS1JW',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   ));
  // GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
   runApp(const MyApp());
