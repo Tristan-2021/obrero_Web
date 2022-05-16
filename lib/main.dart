749dc2f41e85ef31aad27df9371d7b325ab3d178
jhon <Jhonmilerrons@outlook.eso>
2022-05-15 22:02:28 -0500

    delte

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
 import 'example/homeApp/myapp.dart';
 
-
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //     options: const FirebaseOptions(
-  //   apiKey: '',
-  //   authDomain: '',
-  //   projectId: '',
-  //   storageBucket: '',
-  //   messagingSenderId: '',
-  //   appId: '',
-  //   measurementId: '',
-  // ));
   runApp(const MyApp());
 }
